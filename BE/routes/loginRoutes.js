6343295cdbfedb821017664f70b72b9e8a120fbf
Lintang <lintangmuhammadshiddiq@gmail.com>
2024-11-25 14:33:37 +0700

    Initial commit

--- BE/routes/loginRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/loginRoutes.js
-const express = require('express');
-const router = express.Router();
-const loginController = require('../controllers/loginController');
-// const { route } = require('./userRoutes');
-
-router.post('/login', loginController.login);
-
-module.exports = router;
